a0562458e0491e8652e625d5df22dff1153aba56
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-10 06:21:54 +0200

    run server

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// const fileRoutes = require('./routes/fileroutes');
+const path = require('path');
+// const File = require('./models/fileModel');
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+// app.use('/api', fileRoutes);
+app.use(express.static('public'));
+
+// app.get('/file-count', async (req, res) => {
+//     try {
+//         const filesAmount = await File.countDocuments();
+//         res.json({ count: filesAmount });
+//     } catch (error) {
+//         res.status(500).json({ error: 'Error fetching file count' });
+//     }
+// });
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
